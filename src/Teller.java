2c3032317d6dc6bd4c6079b63e37ec8be9be76f5
levin2336 <levin-janssen@web.de>
2022-11-09 10:39:01 +0100

    Teller

--- src/Teller.java
+++ src/Teller.java
@@ -1,2 +1,11 @@
 public class Teller {
+    private boolean flacherTeller;
+
+    public Teller(boolean flacherTeller) {
+        this.flacherTeller = flacherTeller;
+    }
+
+    public boolean getFlacherTeller() {
+        return flacherTeller;
+    }
 }
